abf00b25ad30102e30ba2f512f44df585cf85501
MrYogirs <109125442+MrYogirs@users.noreply.github.com>
2024-03-06 15:55:15 +0800

    YRS

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,8 +1,33 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
 import './Tab2.css';
 import { URL } from '../App';
+import { useState } from 'react';
+import axios from 'axios';
+
+interface DetailRiwayat {
+  matakuliah: string;
+  nama_kelas: string;
+}
 
 const Tab2: React.FC = () => {
+  const [detailRiwayat, setDetailRiwayat] = useState<[DetailRiwayat]> ([{
+    matakuliah: "",
+    nama_kelas: "",
+  }]);
+
+  useIonViewDidEnter(() => {
+    axios.get(`${URL.detailRiwayat}&semid=20231`).then((res) => {
+      if (res.status == 200) {
+        if (res.data.code == 200) {
+          const resultData = res.data.data;
+          setDetailRiwayat(resultData);
+        }
+      }
+    }).catch((err) => {
+      console.log(err);
+    }
+    );
+  })
   return (
     <IonPage>
       <IonHeader>
@@ -10,20 +35,22 @@ const Tab2: React.FC = () => {
           <IonTitle>DETAIL RIWAYAT MENGAJAR</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>         
-        <IonCard color={'light'} href={URL.tab2}>          
-          <IonCardHeader color={'dark'}>
-            <IonCardTitle className='JudulJadwal'>
-              <IonText color={'light'}><h6>Semester yang dipilih</h6></IonText>
-            </IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent className='Konten'>
-            <IonText>Nama_kelas</IonText><br></br>
-            <IonText>Matakuliah</IonText><br></br>
-            <IonText>Prodi</IonText><br></br>
-            <IonText>SKS</IonText>
-          </IonCardContent>
-        </IonCard>
+      <IonContent fullscreen> 
+        {detailRiwayat.map((row: DetailRiwayat, index: number) =>(          
+          <IonCard color={'light'}>          
+            <IonCardHeader color={'dark'}>
+              <IonCardTitle className='JudulJadwal'>
+                <IonText color={'light'}><h6>{row.matakuliah}</h6></IonText>
+              </IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent className='Konten'>
+              <IonText>{row.nama_kelas}</IonText><br></br>
+              <IonText>Matakuliah</IonText><br></br>
+              <IonText>Prodi</IonText><br></br>
+              <IonText>SKS</IonText>
+            </IonCardContent>
+          </IonCard>
+        ))}
       </IonContent>
     </IonPage>
   );
